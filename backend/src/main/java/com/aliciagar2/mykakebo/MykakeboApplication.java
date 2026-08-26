1d232eaedca47fa25e0bda18267fe4ecfb43ff23
aliciagar2 <aliciagar2@gmail.com>
2026-08-26 15:03:34 +0200

    feat: generate Spring Boot skeleton and migrate domain/service

---
+++ backend/src/main/java/com/aliciagar2/mykakebo/MykakeboApplication.java
@@ -0,0 +1,13 @@
+package com.aliciagar2.mykakebo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykakeboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MykakeboApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aliciagar2/mykakebo/MykakeboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliciagar2.mykakebo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MykakeboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
